d3319c67fafbec72c221e4361e28014cee49f99d
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-11-27 19:54:27 +0000

    Add product templet and views

--- bake_boutique/urls.py
+++ bake_boutique/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Admin site
     path('accounts/', include('allauth.urls')),  # django-allauth URLs
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
