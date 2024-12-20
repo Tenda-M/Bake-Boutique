b3406a796c962c1769929459635979c39d603df5
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-12-20 16:22:03 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QY64aJzMFHkUexIHDGpe6Wz4n6wF1oTTQeSOe85CC1JPCAQa6WutlGdfdCBZqCb98JKxilRY1a7WYNTIjh75uHt00Fc07gOT6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
